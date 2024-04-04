3e1a7273649da17dcd61339144155d7b6bae1ae4
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-04-04 15:56:51 +0800

    Add DeleteBookCommandTest

---
+++ src/test/java/seedu/address/logic/commands/DeleteBookCommandTest.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class DeleteBookCommandTest {
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testExecute() {
+
+    }
+}
