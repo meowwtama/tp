35c79a2d03d946e97e6a0cce4267471c41847ffa
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-04-04 14:27:59 +0800

    Add AddBookCommand

---
+++ src/test/java/seedu/address/logic/commands/AddBookCommandTest.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class AddBookCommandTest {
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testExecute() {
+
+    }
+}
