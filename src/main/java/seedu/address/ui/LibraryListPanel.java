d2362eafd261269c67fefa20a2da057664e24211
suraj.shenoy.b@gmail.com <suraj.shenoy.b@gmail.com>
2024-04-04 13:51:43 +0800

    Remove Unnecessary imports

--- src/main/java/seedu/address/ui/LibraryListPanel.java
+++ src/main/java/seedu/address/ui/LibraryListPanel.java
@@ -4,10 +4,8 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.book.Book;
