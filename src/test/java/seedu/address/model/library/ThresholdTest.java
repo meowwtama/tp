d05227a8166074cad86ddbab9c4dd89eabaa626d
ashleyclx <e0958221@u.nus.edu>
2024-04-11 15:09:27 +0800

    Create Threshold test class

---
+++ src/test/java/seedu/address/model/library/ThresholdTest.java
@@ -0,0 +1,24 @@
+package seedu.address.model.library;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ThresholdTest {
+
+    @Test
+    void getThreshold() {
+    }
+
+    @Test
+    void isLessThanOrEqualTo() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
